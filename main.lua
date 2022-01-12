60dec2958be405a31c897c69e4b1960a662cb3c1
Nick M <60850929+wariomale@users.noreply.github.com>
2022-01-12 09:39:00 +0000

    Logged in

--- main.lua
+++ main.lua
@@ -34,11 +34,7 @@ function love.load()
 
     dialogue = {}
     dialogue.demo = {}
-    dialogue.demo.1 = 'Hello World!'
-    dialogue.demo.2 = 'Did you hear about lua?''
-    dialogue.demo.3 = 'They say its pretty cool...'
     dialogue.hasSkipped = false
-
     walls = {}
     if gameMap.layers["Walls"] then
       for i, obj in pairs(gameMap.layers["Walls"].objects) do
@@ -48,83 +44,63 @@ function love.load()
       end
     end
 end
-
 function love.update(dt)
     local isMoving = false
-
     local vx = 0
     local vy = 0
-
     if love.keyboard.isDown("d") then
         vx = player.speed
         player.anim = player.animations.right
         isMoving = true
     end
-
     if love.keyboard.isDown("a") then
         vx = player.speed * -1
         player.anim = player.animations.left
         isMoving = true
     end
-
     if love.keyboard.isDown("s") then
         vy = player.speed
         player.anim = player.animations.down
         isMoving = true
     end
-
     if love.keyboard.isDown("w") then
         vy = player.speed * -1
         player.anim = player.animations.up
         isMoving = true
     end
-
     if vy == (player.speed) or (player.speed * -1) then
       -- vx = 0
     end
-
     player.collider:setLinearVelocity(vx, vy)
-
     if isMoving == false then
         player.anim:gotoFrame(2)
     end
-
     world:update(dt)
     player.x = player.collider:getX()
     player.y = player.collider:getY()
-
     player.anim:update(dt)
-
     cam:lookAt(player.x, player.y)
-
     local w = love.graphics.getWidth()
     local h = love.graphics.getHeight()
-
     -- Left border
     if cam.x < w/2 then
       cam.x = w/2
     end
-
     -- Upper border
     if cam.y < h/2 then
       cam.y = h/2
     end
-
     local mapW = gameMap.width * gameMap.tilewidth
     local mapH = gameMap.height * gameMap.tileheight
-
     -- Right border
     if cam.x > (mapW - w/2) then
       cam.x = (mapW - w/2)
     end
-
     -- Bottom border
     if cam.y > (mapH - h/2) then
       cam.y = (mapH - h/2)
     end
-
 end
-
 function love.draw()
     -- game objects
     cam:attach()
